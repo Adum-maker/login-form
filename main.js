a7fac439f1a63a09a93ebf6aca1660327d3db812
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2023-09-08 19:39:16 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if (username === "admin" && password === "adminpassword") {
+      alert("Login successful");
+    } else if (username === "user" && password === "userpassword") {
+      alert("Login successful");
+    } else {
+      alert("Login failed");
+    }
+  });
+  
